aa15067f708e067dbd97a7a4d921c54dfb08891e
Drew <milukaj@mail.uc.edu>
2026-02-06 11:50:11 -0500

    auth0 and homepage start

--- frontend/data-safe/app/providers.tsx
+++ frontend/data-safe/app/providers.tsx
@@ -4,19 +4,22 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [redirectUri, setRedirectUri] = useState<string>("");
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setRedirectUri(window.location.origin);
+    setMounted(true);
   }, []);
-
-  if (!redirectUri) return <>{children}</>;
+  if (!mounted) return null;
 
   return (
     <Auth0Provider
       domain="dev-t2e2x52h4gs4vmrb.us.auth0.com"
       clientId="qHrandEAgVyXanloFVlMcMrTaJApGUfO"
-      authorizationParams={{ redirect_uri: redirectUri }}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+      // This ensures the cache is stored in local storage, preventing login loss on refresh
+      cacheLocation="localstorage"
     >
       {children}
     </Auth0Provider>
